5eb7868bec7018d2e389d4c6477193bfbe6ed349
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2021-09-23 11:59:56 -0400

    add more error handling

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,65 +1,79 @@
 const express = require("express");
 const Job = require("../models/Job");
 
+// Require handleValidateId by destructuring it from the exports object
+const { handleValidateId, handleRecordExists } = require("../middleware/custom_errors");
+
 const router = express.Router();
 
 // INDEX
 // GET api/jobs
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   // Use our Job model to find all of the documents
   // in the jobs collection
   // When found, they are passed down the promise chain
   // to the `.then()` where we send the response as JSON
   // with `res.json` and pass it any jobs found
-  Job.find().then((jobs) => res.json(jobs));
+  Job.find()
+    .then((jobs) => res.json(jobs))
+    .catch(next);
 });
 
 // SHOW
 // GET api/jobs/5a7db6c74d55bc51bdf39793
-router.get("/:id", (req, res) => {
-  Job.findById(req.params.id).then((job) => {
-    if (!job) {
-      res.send(404);
-    } else {
-      res.json(job);
-    }
-  });
+router.get("/:id", handleValidateId, (req, res, next) => {
+  Job.findById(req.params.id)
+    .then(handleRecordExists)
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.json(job);
+      }
+    })
+    .catch(next);
 });
 
 // CREATE
 // POST api/jobs
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   Job.create(req.body)
-    .status(201)
-    .then((job) => res.json(job));
+    .then((job) => res.json(job))
+    .catch(next);
 });
 
 // UPDATE
 // PUT api/jobs/5a7db6c74d55bc51bdf39793
-router.put("/:id", (req, res) => {
+router.put("/:id", handleValidateId, (req, res, next) => {
   Job.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true
-  }).then((job) => {
-    if (!job) {
-      res.sendStatus(404);
-    } else {
-      res.json(job);
-    }
-  });
+  })
+    .then(handleRecordExists)
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.json(job);
+      }
+    })
+    .catch(next);
 });
 
 // DESTROY
 // DELETE api/jobs/5a7db6c74d55bc51bdf39793
-router.delete("/:id", (req, res) => {
+router.delete("/:id", handleValidateId, (req, res, next) => {
   Job.findOneAndDelete({
     _id: req.params.id
-  }).then((job) => {
-    if (!job) {
-      res.sendStatus(404);
-    } else {
-      res.json(204);
-    }
-  });
+  })
+    .then(handleRecordExists)
+    .then((job) => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.json(204);
+      }
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { handleErrors, handleValidationErrors } = require("./middleware/custom_errors");
 
 // Instantiate express application object
 const app = express();
@@ -26,6 +27,21 @@ const jobController = require("./controllers/jobs");
 
 // Configure the route middleware
 app.use("/api/jobs", jobController);
+
+// The last middleware receives any error as its first argument
+// app.use((err, req, res, next) => {
+//   // If the error contains a statusCode, set the variable to that code
+//   // if not, set it to a default 500 code
+//   const statusCode = err.statusCode || 500;
+//   // If the error contains a message, set the variable to that message
+//   // if not, set it to a generic 'Internal Server Error'
+//   const message = err.message || "Internal Server Error";
+//   // Set the status and send the message as a response to the client
+//   res.status(statusCode).send(message);
+// });
+
+app.use(handleValidationErrors);
+app.use(handleErrors);
 
 // Define a port for API to run on, if the environment
 // variable called `PORT` is not found use port 4000
